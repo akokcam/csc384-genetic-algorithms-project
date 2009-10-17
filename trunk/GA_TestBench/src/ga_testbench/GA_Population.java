71334638c24ff429403c241aa22b70bb1f2e0058
davekordalewski@gmail.com <davekordalewski@gmail.com@738b5d3a-ba83-11de-bda7-396a5af1c03d>
2009-10-17 08:18:48 +0000

    comments, tiny

--- trunk/GA_TestBench/src/ga_testbench/GA_Population.java
+++ trunk/GA_TestBench/src/ga_testbench/GA_Population.java
@@ -4,6 +4,13 @@ interface GA_Population<SolutionInstance extends GA_Individual> {
 
     public SolutionInstance getBest();
 
+    /* This should be weighted by the normalized fitness. I can't think of how
+     * to implement a nice quick way to select from the population besides
+     * storing them in an augmented interval tree. But I just learned about
+     * this in 265. Am I missing an obvious simple way to do it?
+     * */
+    public SolutionInstance getRandom();
+
     /* Some simple options. These should be enough for most purposes for the
      * population module. Just tells it what weights it should place on the
      * different things for the new generation
