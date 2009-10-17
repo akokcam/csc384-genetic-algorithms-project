8127049ea1a003cf930218aacdea647cd22285fb
davekordalewski@gmail.com <davekordalewski@gmail.com@738b5d3a-ba83-11de-bda7-396a5af1c03d>
2009-10-17 07:24:05 +0000

    first add

---
+++ trunk/GA_TestBench/src/ga_testbench/GA_Individual.java
@@ -0,0 +1,14 @@
+package ga_testbench;
+
+public interface GA_Individual {
+
+    public GA_Individual random();
+
+    public float evaluate();
+
+    public float pairEvaluate(GA_Individual other);
+
+    public void mutate(float difference);
+
+    public GA_Individual crossWith(GA_Individual other);
+}
---
+++ trunk/GA_TestBench/src/ga_testbench/GA_Solver.java
@@ -0,0 +1,6 @@
+package ga_testbench;
+
+public class GA_Solver<T extends GA_Individual> {
+    GA_Population<T> pop;
+
+}
