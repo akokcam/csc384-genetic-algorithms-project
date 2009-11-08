420fd61c46b069aef4cbb9023ab58404fa5842c0
davekordalewski <davekordalewski@738b5d3a-ba83-11de-bda7-396a5af1c03d>
2009-11-08 18:47:37 +0000

    ScheduleInstance generator made, provisional mutate and crossover operators established

---
+++ GA_TestBench/src/ScheduleInstanceGenerator/Main.java
@@ -0,0 +1,73 @@
+package ScheduleInstanceGenerator;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * This is used to geenrate Scheduling problem instances with desired
+ * properties. Just change the static final ints at the top.
+ * @author dave
+ */
+public class Main {
+
+    public static final String FILENAME = "Instance Input Files\\tickle.txt";
+    public static final int NUMDAYS = 5;
+    public static final int NUMTIMES = 5;
+    public static final int NUMCOURSES = 20;
+    public static final int NUMSTUDENTS = 20;
+    public static final int NUMROOMS = 4;
+    public static final int MAXCOURSESPERSTUDENT = 6;
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        try {
+
+            FileWriter fstream = new FileWriter(FILENAME);
+            BufferedWriter out = new BufferedWriter(fstream);
+//            writeStream out = System.out;
+            out.write(NUMDAYS + " " + NUMTIMES + "\n");
+
+            out.write("\n" + NUMCOURSES + "\n");
+            for (int c = 1; c <= NUMCOURSES; c++) {
+                out.write("Course " + c + "\n");
+            }
+
+            out.write("\n" + NUMROOMS + "\n");
+            for (int r = 1; r <= NUMROOMS; r++) {
+                out.write("Room " + r + "\n");
+            }
+
+            out.write("\n" + NUMSTUDENTS + "\n");
+            for (int i = 1; i <= NUMSTUDENTS; i++) {
+                out.write("Student " + i + "\n");
+                // Tend toward more courses with a sqrt
+                int numCourses = (int) (Math.sqrt(rand.nextFloat()) * MAXCOURSESPERSTUDENT);
+                if (numCourses == 0) {
+                    numCourses = 1;
+                }
+                out.write(numCourses + "\n");
+                Set cs = new TreeSet();
+                for (int k = 0; k < numCourses; k++) {
+                    int coursenum = (int) (1 + rand.nextFloat() * NUMCOURSES);
+                    if (cs.contains(coursenum)) {
+                        k--;
+                    } else {
+                        out.write("Course " + coursenum + "\n");
+                        cs.add(coursenum);
+                    }
+                }
+            }
+
+
+            out.close();
+        } catch (IOException e) {
+        }
+    }
+}
--- GA_TestBench/src/ScheduleProblem/Main.java
+++ GA_TestBench/src/ScheduleProblem/Main.java
@@ -1,7 +1,5 @@
 package ScheduleProblem;
 
-import ga_testbench.Individual;
-
 public class Main {
 
     /**
@@ -10,37 +8,63 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        boolean loadedOK = Schedule.initialize("Instance Input Files\\testinitfile.txt");
+        boolean loadedOK = Schedule.initialize("Instance Input Files\\tickle.txt");
 
         if (loadedOK) {
-            Schedule.displayInfo();
+            //Schedule.displayInfo();
+        } else {
+            throw new RuntimeException("Schedule failed to load.");
         }
 
         System.out.println("\nWe have now generated a random schedule.");
 
         Schedule ss = (Schedule) Schedule.random();
-        System.out.println(ss);
+//        System.out.println(ss);
+//        System.out.println(ss.studentSchedulesString());
+//        System.out.println(ss.roomSchedulesString());
 
-        System.out.println(ss.studentSchedulesString());
-        System.out.println(ss.roomSchedulesString());
 
+        /*
+        // This is for checking a problem instance, generating a few solutions
+        // and seeing a decent one
         float fitness = ss.fitness();
         System.out.println("The fitness function of this schedule gives: " + fitness);
 
-/* the code below is to test schedule's clone method.
+        int trials = 20000;
+        float tot = 0;
+        Schedule best = null;
+        float highestf = 0;
+        for (int i = 0; i < trials; i++) {
+        Schedule r = (Schedule) Schedule.random();
+        float f = r.fitness();
+        if (f > highestf) {
+        highestf = f;
+        best = r;
+        }
+        tot += f;
+        }
+        System.out.println("Average fitness: " + (tot / trials));
+
+        System.out.println(best);
+        System.out.println(best.studentSchedulesString());
+        System.out.println(best.roomSchedulesString());
+        System.out.println("The fitness function of this schedule gives: " + best.fitness());
+         */
+
+        /* the code below is to test schedule's clone method.
         Schedule c;
         try{
-            c = ss.clone();
-            System.out.println("\n****************testing c************\n");
-            System.out.println(c);
+        c = ss.clone();
+        System.out.println("\n****************testing c************\n");
+        System.out.println(c);
 
-            System.out.println(c.studentSchedulesString());
-            System.out.println(c.roomSchedulesString());
+        System.out.println(c.studentSchedulesString());
+        System.out.println(c.roomSchedulesString());
 
-            fitness = c.fitness();
-            System.out.println("The fitness function of this schedule gives: " + fitness);
+        fitness = c.fitness();
+        System.out.println("The fitness function of this schedule gives: " + fitness);
         }
         catch(Exception e){}
-      */
+         */
     }
 }
